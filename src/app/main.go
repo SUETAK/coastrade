5c79f735733e20e4a0bc188603cc5de958702ce5
SUETAK <akasirotonbo08451@gmail.com>
2021-04-24 23:40:36 +0900

    docker-compose 立ち上げ時にmysqlに接続する

--- src/app/main.go
+++ src/app/main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
-	"log"
 	"fmt"
-
-	"github.com/geek-line/sum"
+	"time"
+	"github.com/jinzhu/gorm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Println("Hello Go!")
-	result := sum.Sum(2, 3)
-	log.Print(result)
+	db := sqlConnect()
+	defer db.Close()
+}
+
+func sqlConnect() (database *gorm.DB) {
+	DBMS := "mysql"
+	USER := "suetak"
+	PASSWORD := "suetak"
+	PROTOCOL := "tcp(db:3306)"
+	DBNAME := "crypto"
+	
+	CONNECT := USER + ":" + PASSWORD + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+
+	count := 0
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		for {
+			if err == nil {
+				fmt.Println("")
+				break
+			}
+			fmt.Print(".")
+			time.Sleep(time.Second)
+			count++
+			if count>60 {
+				fmt.Print("")
+				fmt.Print("db接続失敗")
+				panic(err)
+			}
+			db, err = gorm.Open(DBMS, CONNECT)
+		}
+	}
+	fmt.Println("DB接続成功")
+	return db
 }
