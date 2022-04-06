3c945774e4dcc0b34ab9b1406fdc24a502bfe4b2
SUETAK <akasirotonbo08451@gmail.com>
2022-04-06 22:56:52 +0900

    candle 関連用ファイルを作成

--- usecase/candle.go
+++ usecase/candle.go
@@ -3,4 +3,12 @@ package usecase
 // Candle についてのロジックを提供する
 
 type CandleUseCase interface {
+	Answer() (string, error)
+}
+
+type CandleUsecase struct {
+}
+
+func (receiver CandleUsecase) Answer() (string, error) {
+	return "Answer関数が呼ばれました", nil
 }
